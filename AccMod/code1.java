37772c92fccb0422079cb607b45583ddbf21828f
unknown <gdutt2304@gmail.com>
2024-09-17 19:13:25 +0530

    Access modifiers public

---
+++ AccMod/code1.java
@@ -0,0 +1,16 @@
+package AccMod;
+
+public class code1 {
+    public String name;
+    public int age;
+
+    public code1(String name, int age) {
+      this.name = name;
+      this.age = age;
+    }
+  public void Display() {
+      System.out.println("Name : " + name);
+      System.out.println("Age : " + age);
+  }
+
+}
---
+++ AccMod/code2.java
@@ -0,0 +1,8 @@
+package AccMod;
+
+public class code2 {
+    public static void main(String[] args) {
+        code1 obj = new code1("Shivam Bhai " , 19);
+        obj.Display();
+    }
+}
